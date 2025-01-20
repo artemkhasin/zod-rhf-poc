9a67c6daead2d52d384c81658a27aca659266316
artemkhasin <artem.khasin@aizon.ai>
2025-01-20 12:01:36 +0100

    feat: form component instead of hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,19 @@
 import { useTranslation } from 'react-i18next';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
 import {
-  Box,
-  Button,
   Typography,
   Paper,
 } from '@mui/material';
-import { FormData, zodSchema } from './schema';
+import { zodSchema, type FormData } from './schema';
 import JSONFormSchema from './schema/form.json';
-import { useFormFactory } from './components/hooks/useFormFactory';
-import { FormProperty } from './schema/types';
-import React from 'react';
 import LanguageSelector from './components/languageSelector/LanguageSelector';
+import { FormComponent } from './components/form';
 
 
 
 function App() {
   const { t  } = useTranslation();
-  const { renderFormInput } = useFormFactory<FormData>();
   const {title, properties } = JSONFormSchema;
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(zodSchema),
-    defaultValues: {
-      active: true,
-      tags: [],
-    },
-  });
-
-  const onSubmit = (data: FormData) => {
-    console.log(data);
-  };
+  console.log(properties);
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
@@ -42,19 +21,10 @@ function App() {
         <Typography variant="h4" component="h1" gutterBottom>
           {t(title)}
         </Typography>
-        
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            {Object.entries(properties).map(([key, property]: [string, FormProperty]) => 
-              <React.Fragment key={key}>
-                {renderFormInput(key, property, control, errors)}
-              </React.Fragment>
-              )}
-          <Box className="pt-4">
-            <Button type="submit" variant="contained" color="primary">
-              {t('Submit')}
-            </Button>
-          </Box>
-        </form>
+        <FormComponent<FormData> 
+          ZodFormSchema={zodSchema} 
+          JSONFormSchemaProperties={properties} 
+        />
       </Paper>
       <LanguageSelector />
     </div>
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,8 +1,15 @@
+import React from "react";
+import { z } from "zod";
+import { Button, Box } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useForm, Control, FieldErrors } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
 import FormInput from "./FormInput";
 import FormDatePicker from "./FormDatePicker";
 import FormTextArea from "./FormTextArea";
 import FormToggle from "./FormToggle";
 import FormSelector from "./FormSelector";
+import { FormProperty } from "../../schema/types";
 
 export { 
     FormInput, 
@@ -10,4 +17,73 @@ export {
     FormTextArea, 
     FormToggle, 
     FormSelector 
+};
+
+enum InputType {
+    STRING = 'input',
+    DATE_PICKER = 'date',
+    TEXT_AREA = 'textarea',
+    TOGGLE = 'toggle',
+    SELECTOR = 'selector'
+}
+
+interface FormComponentProps<U> {
+    ZodFormSchema: z.ZodTypeAny,
+    JSONFormSchemaProperties: { [key: string]: FormProperty  },
+}
+
+const AVAILABLE_TAGS = ['Important', 'Urgent', 'Review', 'Draft', 'Final'];
+
+export const FormComponent = ({ZodFormSchema, JSONFormSchemaProperties }: FormComponentProps<U>) => {
+    
+    const {
+            control,
+            handleSubmit,
+            formState: { errors },
+        } = useForm<U>({
+            resolver: zodResolver(ZodFormSchema),
+            defaultValues: {
+                active: true,
+                tags: [],
+            },
+    });
+
+    const { t } = useTranslation();
+
+    const onSubmit = (data: U) => {
+        console.log(data);
+    };
+    
+    const renderFormInput = (name: string, property: FormProperty, control: Control<U>, errors: FieldErrors<U>) => {
+        const {inputType, title } = property;
+        switch (inputType) {
+            case InputType.STRING:
+                return <FormInput name={name} label={title} control={control} errors={errors} t={t} />;
+            case InputType.DATE_PICKER:
+                return <FormDatePicker name={name} label={title} control={control} errors={errors} t={t} />
+            case InputType.TEXT_AREA:
+                return <FormTextArea name={name} label={title} control={control} errors={errors} t={t} />
+            case InputType.TOGGLE:
+                return <FormToggle name={name} label={title} control={control} errors={errors} t={t} />
+            case InputType.SELECTOR:
+                return <FormSelector name={name} label={title} control={control} errors={errors} tags={AVAILABLE_TAGS} t={t} />
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            {Object.entries(JSONFormSchemaProperties).map(([key, property]: [string, FormProperty]) => 
+                <React.Fragment key={key}>
+                    {renderFormInput(key, property, control, errors)}
+                </React.Fragment>
+                )}
+            <Box className="pt-4">
+                <Button type="submit" variant="contained" color="primary">
+                    {t('Submit')}
+                </Button>
+            </Box>
+        </form>
+    )
 };
